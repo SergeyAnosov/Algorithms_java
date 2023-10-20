df632d48d47a23631e29ae4d876d058181c70d7c
Sergey Anosov <Anosov-sa@yandex.ru>
2023-10-20 09:16:50 +0300

    bubble sort

---
+++ bubble/src/main/java/Bubble_sort.java
@@ -0,0 +1,17 @@
+public class Bubble_sort {
+
+    public int[] sort(int[] a) {
+
+        for (int i = a.length - 1; i > 1; i --) {
+            for (int j = 0; j < i; j++) {
+                int temp;
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        return a;
+    }
+}
