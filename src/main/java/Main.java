26c06da5193867c6e5a13ac0999636e912455875
Sergey Anosov <Anosov-sa@yandex.ru>
2023-10-23 16:24:09 +0300

    Insertion_sort

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Selection_sort ss = new Selection_sort();
+        Insertion_sort s = new Insertion_sort();
 
         int[] ar1 = {5, 7, 3, 1, 0, 10, 0};
         int[] ar2 = {9, 7, 13, 1, -5, 10, 0, 17, 43};
 
-        System.out.println(Arrays.toString(ss.selectionSort(ar1)));
+        System.out.println(Arrays.toString(s.insertionSort(ar1)));
         System.out.println("--------------------------");
-        System.out.println(Arrays.toString(ss.selectionSort(ar2)));
+        System.out.println(Arrays.toString(s.insertionSort(ar2)));
     }
 }
